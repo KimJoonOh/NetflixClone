c3a3278f7a2771c5628ad8452d19bb4756ed7930
Joon <kjo11300@naver.com>
2021-07-16 15:33:21 +0900

    서치바 생성 및 키보드 내림 설정

--- MyNetflix/SearchViewController.swift
+++ MyNetflix/SearchViewController.swift
@@ -10,8 +10,38 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var resultCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // 검색 시작
+        
+        
+        
+        // 키보드 내려가게 처리
+        dismissKeyboard()
+        // 검색어가 있는지
+        
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else {return}
+        
+        
+        
+        // 네트워킹을 통한 검색
+        print("--> 검색어 : \(searchBar.text)")
+        
+        
     }
     
 }
