d1556ac30107eb1bbca1a078b6ea2dcf18eed505
Joon <kjo11300@naver.com>
2021-07-16 23:07:45 +0900

    영화 검색내용 보여줄 콜렉션뷰 생성 및 셀 정의

--- MyNetflix/SearchViewController.swift
+++ MyNetflix/SearchViewController.swift
@@ -13,11 +13,58 @@ class SearchViewController: UIViewController {
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var resultCollectionView: UICollectionView!
     
+    var movies: [Movie] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultCollectionView.register(ResultCell.self, forCellWithReuseIdentifier: "ResultCell")
     }
     
 }
+
+extension SearchViewController: UICollectionViewDataSource {
+    // 몇개넘어오니?
+    
+    // 어떻게 표현?
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else {
+            return UICollectionViewCell()
+        }
+        
+        let movie = movies[indexPath.item]
+//        cell.movieThumbnail.image =
+        
+        cell.backgroundColor = .red
+        return cell
+    }
+}
+
+class ResultCell: UICollectionViewCell {
+    @IBOutlet var movieThumbnail: UIImageView!
+    
+}
+
+extension SearchViewController: UICollectionViewDelegate {
+    
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 8
+        let itemSpacing: CGFloat = 10
+        
+        let width = (collectionView.bounds.width - margin * 2 - itemSpacing * 2) / 3
+        let height = width * 10 / 7
+        return CGSize(width: width, height: height)
+        
+    }
+}
+
 
 extension SearchViewController: UISearchBarDelegate {
     
@@ -36,6 +83,15 @@ extension SearchViewController: UISearchBarDelegate {
         
         guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else {return}
         
+        SearchAPI.search(searchTerm) { movies in
+            // collectionview로 표현하기
+            print(" 몇개넘어왔나? \(movies.count)")
+            self.movies = movies
+            DispatchQueue.main.async {
+                self.movies = movies
+                self.resultCollectionView.reloadData()
+            }
+        }
         
         
         // 네트워킹을 통한 검색
@@ -43,5 +99,81 @@ extension SearchViewController: UISearchBarDelegate {
         
         
     }
+    
+}
+
+class SearchAPI {
+    static func search(_ term:String, completion: @escaping ([Movie]) -> Void) {
+        let session = URLSession(configuration: .default)
+        var urlComponent = URLComponents(string: "https://itunes.apple.com/search?")!
+        let mediaQuery = URLQueryItem(name: "media", value: "movie")
+        let entityQuery = URLQueryItem(name: "entity", value: "movie")
+        let termQuery = URLQueryItem(name: "term", value: term)
+        urlComponent.queryItems?.append(mediaQuery)
+        urlComponent.queryItems?.append(entityQuery)
+        urlComponent.queryItems?.append(termQuery)
+        
+        let requestURL = urlComponent.url!
+        let dataTask = session.dataTask(with: requestURL) { data, response, error in
+            let successRange = 200..<300
+            
+            guard error == nil, let statusCode = (response as? HTTPURLResponse)?.statusCode, successRange.contains(statusCode) else {
+                completion([])
+                return
+            }
+            
+            guard let resultData = data else {
+                completion([])
+                return
+            }
+            
+            // data -> [Movie]
+            let string = String(data: resultData, encoding: .utf8)
+            let movies = SearchAPI.parseMovies(resultData)
+            completion(movies)
+//            print("--> result: \(string)")
+            
+//            completion([Movie])
+        }
+        dataTask.resume()
+    }
+    
+    static func parseMovies(_ data: Data) -> [Movie] {
+        let decoder = JSONDecoder()
+        
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+        } catch let error {
+            print("parse error: \(error.localizedDescription)")
+            return []
+        }
+    }
+}
+
+struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
+    
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
+}
+
+struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
+    
     
 }
