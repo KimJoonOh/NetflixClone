800b1aa8f6bd0efd50ac5236af7d354b6e792df2
Joon <kjo11300@naver.com>
2021-07-17 14:18:15 +0900

    av player 클래스 최신 소스로 수정

--- MyNetflix/PlayerView.swift
+++ MyNetflix/PlayerView.swift
@@ -11,22 +11,36 @@
 import UIKit
 import AVFoundation
 
+//class PlayerView: UIView {
+//    var player: AVPlayer? {
+//        get {
+//            return playerLayer.player
+//        }
+//        set {
+//            playerLayer.player = newValue
+//        }
+//    }
+//
+//    var playerLayer: AVPlayerLayer {
+//        return layer as! AVPlayerLayer
+//    }
+//
+//    // Override UIView property
+//    override static var layerClass: AnyClass {
+//        return AVPlayerLayer.self
+//    }
+//}
+
 class PlayerView: UIView {
+
+    // Override the property to make AVPlayerLayer the view's backing layer.
+    override static var layerClass: AnyClass { AVPlayerLayer.self }
+
+    // The associated player object.
     var player: AVPlayer? {
-        get {
-            return playerLayer.player
-        }
-        set {
-            playerLayer.player = newValue
-        }
+        get { playerLayer.player }
+        set { playerLayer.player = newValue }
     }
-    
-    var playerLayer: AVPlayerLayer {
-        return layer as! AVPlayerLayer
-    }
-    
-    // Override UIView property
-    override static var layerClass: AnyClass {
-        return AVPlayerLayer.self
-    }
+
+    private var playerLayer: AVPlayerLayer { layer as! AVPlayerLayer }
 }
